d1fc06893d6212c7b11ee06d0298e27e75be525b
ThamAdi <71882203+ThamAdi@users.noreply.github.com>
2022-09-05 11:07:06 +0530

    soap-webservice

---
+++ SOAP-Webservice/Application.java
@@ -0,0 +1,11 @@
+package com.tagamage.soap;
+
+public class Application {
+	
+	Person p=new Person();
+	public String getdob(String dob) {
+		// TODO Auto-generated method stub
+		return p.getdob().substring(3, 4);
+	}
+
+}
