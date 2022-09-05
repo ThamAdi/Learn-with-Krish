7bd105b9861e6f5360b17248973ad64a6be956d5
ThamAdi <71882203+ThamAdi@users.noreply.github.com>
2022-09-05 11:12:25 +0530

    soap-webservice commit

---
+++ SOAP-webservice/java/com/tagamage/soap/Person.java
@@ -0,0 +1,18 @@
+package com.tagamage.soap;
+
+
+public class Person{
+	
+	private String dob;
+	
+	public String getdob() {
+		return dob;
+	}
+
+	public void setdob(String dob) {
+		this.dob = dob;
+	}
+
+}
+
+
